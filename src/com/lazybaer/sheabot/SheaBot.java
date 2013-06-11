2004700b01985a56e5e297ef43f008670d0eb264
CWD <cwd@lazybaer.com>
2013-06-10 22:51:06 -0400

    testing testing

--- src/com/lazybaer/sheabot/SheaBot.java
+++ src/com/lazybaer/sheabot/SheaBot.java
@@ -3,6 +3,7 @@ package com.lazybaer.sheabot;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import org.jivesoftware.smack.Chat;
@@ -18,19 +19,21 @@ public class SheaBot
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)
+	
+	public SheaBot()
 	{
-		
 		try
 		{
 			Properties prop = new Properties();
-			prop.load(new FileInputStream("config.properties"));
+			InputStream is=getClass().getResourceAsStream("/config.properties");
+			prop.load(is);
 			
 			Connection connection = new XMPPConnection("gmail.com");
 			System.out.println("firing up conn...");
 			connection.connect();
-			System.out.println("logging in...");
-			connection.login(prop.getProperty("username"), prop.getProperty("password"));
+			String strUser=prop.getProperty("username");
+			System.out.println("logging in as: "+strUser);
+			connection.login(strUser, prop.getProperty("password"));
 			Chat chat = connection.getChatManager().createChat("j.fidlow@gmail.com", new MessageListener() {
 
 			    public void processMessage(Chat chat, Message message) {
@@ -51,8 +54,12 @@ public class SheaBot
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
+		}		
+	}
+	
+	public static void main(String[] args)
+	{
+		new SheaBot();		
 	}
 
 }
