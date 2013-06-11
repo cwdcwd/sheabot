99fb1228d417a1aafd8c6658398397eb3916c4f9
CWD <cwd@lazybaer.com>
2013-06-10 22:20:22 -0400

    main shell

---
+++ src/com/lazybaer/sheabot/SheaBot.java
@@ -0,0 +1,15 @@
+package com.lazybaer.sheabot;
+
+public class SheaBot
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
